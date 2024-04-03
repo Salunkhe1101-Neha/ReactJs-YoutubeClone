095eb712b0de9a86363cc26486a5c2fce826d983
Salunkhe1101-Neha <salunkheneha174@gmail.com>
2024-04-03 17:30:57 +0530

    configured tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from './Components/Head'
+import Body from './Components/Body'
+const App = () => {
+  return (
+    <div>
+       <Head/>
+       <Body/>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
